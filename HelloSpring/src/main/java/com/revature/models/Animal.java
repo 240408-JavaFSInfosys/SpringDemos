3781b71db7700254b550c419545071b755ee00eb
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-05-01 14:55:45 -0400

    hello???

--- HelloSpring/src/main/java/com/revature/models/Animal.java
+++ HelloSpring/src/main/java/com/revature/models/Animal.java
@@ -1,124 +1,49 @@
 package com.revature.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+        import org.springframework.context.annotation.Scope;
+        import org.springframework.stereotype.Component;
 
-import java.util.Objects;
+@Component //This is a stereotype annotation - it makes a Class a Spring Bean
+public class Owner {
 
-@Component //We want to make this class a Spring Bean, which @Component lets us do
-@Scope("singleton") //This will allow us to have different instances of Animal (as opposed to Singletons)
-public class Animal {
-
-    private String bafgdf;
-    private int adfgadfgadfgt;
     private String name;
-    private int adfgadfgadfgt;
-    private int adfgadfgadfgt;
-    private String color;
+    private int age;
 
-    private Owner owner; //Owner is a DEPENDENCY of Animal. Every animal has an owner
-
-    //boilerplate code--------------------------
+    //boilerplate code---------------------------------------
 
     //no args constructor
-    public Animal() {
-    }
-
-        //all args constructor
-    public Animal(String type, String name, Owner owner) {
-        this.type = type;
-        this.name = name;
-        this.owner = owner;
+    public Owner() {
     }
 
     //all args constructor
-    public Animal(String type, int weight, String color, Owner owner) {
-        this.type = type;
-        this.weight = weight;
-        this.color = color;
-        this.owner = owner;
-    }
-
-        //all args constructor
-    public Animal(String type, String name, int weight, String color) {
-        this.type = type;
+    public Owner(String name, int age) {
         this.name = name;
-        this.weight = weight;
-        this.color = color;
-    }
-
-    //constructor injection for Owner
-    @Autowired
-    public Animal(Owner owner) {
-        this.owner = owner;
+        this.age = age;
     }
 
     //getters and setters
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-
-    /*Throwing in a random block comment to confuse git further*/
-
-
-    /*Throwing in a random block comment to confuse git further*/
-
-    
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        /*Throwing in a random block comment to confuse git further*/
         this.name = name;
     }
 
-    public int getWeight() {
-        return weight;
+    public int getAge() {
+        return age;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        /*Throwing in a random block comment to confuse git further*/
-        this.color = color;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-
-
-    /*Throwing in a random block comment to confuse git further*/
-
-    //We could have done setter injection too, but I tend to stick with constructor injection
-    //@Autowired
-    public void setOwner(Owner owner) {
-        this.owner = owner;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     //toString
     @Override
     public String toString() {
-        return "Animal{" +
-                "type='" + type + '\'' +
-                ", name='" + name + '\'' +
-                ", weight=" + weight +
-                ", color='" + color + '\'' +
-                ", owner=" + owner +
+        return "Owner{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
                 '}';
     }
 
