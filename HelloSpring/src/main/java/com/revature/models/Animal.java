d640751c1fd387801656a8e7a2ddaf5741a95fd6
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-05-02 01:35:28 -0400

    Update Animal.java

--- HelloSpring/src/main/java/com/revature/models/Animal.java
+++ HelloSpring/src/main/java/com/revature/models/Animal.java
@@ -7,14 +7,12 @@ import org.springframework.stereotype.Component;
 import java.util.Objects;
 
 @Component //We want to make this class a Spring Bean, which @Component lets us do
-@Scope("singleton") //This will allow us to have different instances of Animal (as opposed to Singletons)
+@Scope("prototype") //This will allow us to have different instances of Animal (as opposed to Singletons)
 public class Animal {
 
-    private String bafgdf;
-    private int adfgadfgadfgt;
+    private String type;
     private String name;
-    private int adfgadfgadfgt;
-    private int adfgadfgadfgt;
+    private int weight;
     private String color;
 
     private Owner owner; //Owner is a DEPENDENCY of Animal. Every animal has an owner
@@ -23,6 +21,69 @@ public class Animal {
 
     //no args constructor
     public Animal() {
+    }
+
+    //all args constructor
+    public Animal(String type, String name, int weight, String color, Owner owner) {
+        this.type = type;
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    //constructor injection for Owner
+    @Autowired
+    public Animal(Owner owner) {
+        this.owner = owner;
+    }
+
+    //getters and setters
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    //We could have done setter injection too, but I tend to stick with constructor injection
+    //@Autowired
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    //toString
+    @Override
+    public String toString() {
         return "Animal{" +
                 "type='" + type + '\'' +
                 ", name='" + name + '\'' +
